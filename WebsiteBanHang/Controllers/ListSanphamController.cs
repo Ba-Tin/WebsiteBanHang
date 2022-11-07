521f0824148009bf9857da72b9ca900503829e43
Nguyễn Bá Tín <nguyenbatin290900@gmail.com>
2022-11-07 18:48:53 +0700

    Add project files.

---
+++ WebsiteBanHang/Controllers/ListSanphamController.cs
@@ -0,0 +1,52 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebsiteBanHang.Context;
+
+namespace WebsiteBanHang.Controllers
+{
+  
+
+    public class ListSanphamController : Controller
+    {
+        QLdienthoai db = new QLdienthoai();
+        // GET: ListSanpham
+       
+
+        [HttpGet]
+        public ActionResult Index(string CurrenFiler, int? page, string search)
+        {
+            
+           var sanpham = new List<Sanpham>();
+            if (search != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                search = CurrenFiler;
+            }
+            if (!string.IsNullOrEmpty(search))
+            {
+                sanpham = db.Sanpham.Where(n => n.Tensp.Contains(search)).ToList();
+            }
+            else
+            {
+                sanpham = db.Sanpham.ToList();
+            }
+            ViewBag.CurrenFiler = search;
+            int pageSize = 5;
+            int pageNumber = (page ?? 1);
+            return View(sanpham.ToPagedList(pageNumber, pageSize));
+        }
+        public ActionResult Index(int Mahang)
+        {
+            var listSanpham = db.Sanpham.Where(n => n.Mahang == Mahang).ToList();
+            return View(listSanpham);
+        }
+
+    }
+}
